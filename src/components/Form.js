d3403565f8b5b899ecaed3f658725e960d063bf1
Jaspreet Kaur <jaspreet0336.be20@chitkara.edu.in>
2022-10-12 14:27:13 +0530

    Intial commit

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Cricketers from './Cricketers';
+import './Form.css';
+import { Link, useNavigate } from 'react-router-dom';
+import cricketer from './cricketer.jpg';
+import cup from './cup.png'
+
+
+export default function Form() {
+  const [position, setposition] = useState('');
+  const [player, setplayer] = useState('');
+  const [rating, setrating] = useState('');
+
+  let history = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let a = position, b = player, c = rating;
+    Cricketers.push({ a, b, c });
+    // console.log(Cricketers);
+    history('/added');
+
+
+  }
+
+  return (
+    <div>
+      <h1>Empolyee Feedback</h1>
+      <form className='container'>
+        <label>Position</label><br></br>
+        <input type="text"   placeholder='Enter Position' onChange={(e) => setposition(e.target.value)} required></input><br></br>
+        <label>Name</label><br></br>
+        <input type="text"   placeholder='Enter Name' onChange={(e) => setplayer(e.target.value)} required></input><br></br>
+        <label >Rating</label><br></br>
+        <input type="number" placeholder='Enter Rating' onChange={(e) => setrating(e.target.value)} required></input><br></br>
+        <Link to='/added'>
+          <input className='btn' type="submit" onClick={handleSubmit}></input>
+        </Link>
+
+      </form>
+
+
+    </div>
+  )
+}
